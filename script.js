64bbef68f04c6bdcf7fa6717225912f1599e0a63
Ryan Valentine <ryan@atriadev.com>
2022-02-15 13:28:05 -0700

    mobile nav opening

---
+++ script.js
@@ -0,0 +1,7 @@
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".main-nav");
+
+toggleButton.addEventListener("click", function () {
+  //   mobileNav.classList.add("open");
+  mobileNav.classList.add("open");
+});
