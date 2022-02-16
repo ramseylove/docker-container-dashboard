b5b947904b2cc49ef8ec5375abdde15881e10919
Ryan Valentine <ryan@atriadev.com>
2022-02-15 19:35:30 -0700

    working on vertically aligning header nav

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".main-nav");
 
+var navState = false;
+
 toggleButton.addEventListener("click", function () {
   //   mobileNav.classList.add("open");
-  mobileNav.classList.add("open");
+  if (!navState) {
+    mobileNav.classList.add("open");
+    navState = true;
+  } else {
+    mobileNav.classList.remove("open");
+    navState = false;
+  }
 });
